b069c93ab13d2b242f7413afa8c906a3e3059b39
mscoloma <MaryAnn.Coloma@edu.sait.ca>
2026-09-16 15:34:10 -0600

    error changed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,25 @@
 import { View, Text, Image, Button, Alert, StyleSheet } from "react-native";
+
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.name}>Mary Ann Coloma</Text>
-      <Text style={styles.name}>Jan 24, 2017</Text>
+      <Text style={styles.date}>Jan 24, 2017</Text>
+
       <Image
         source={require("../../assets/myphoto.jpg")}
         style={styles.photo}
-        <View style={styles.row}>
+      />
+
+      <View style={styles.row}>
         <Text style={styles.engagement}>❤️ 347</Text>
         <Text style={styles.engagement}>💬 127</Text>
         <Text style={styles.engagement}>🔁 1</Text>
       </View>
-        
-        <Button
+
+      <Button
         title="Press Me"
         onPress={() => Alert.alert("This is Mary's first assignment!")}
-      />
       />
     </View>
   );
@@ -28,24 +31,24 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   date: {
-    color: 'gray',
+    color: "gray",
     marginBottom: 10,
   },
   photo: {
-    width: '100%',
+    width: "100%",
     height: 400,
     borderRadius: 10,
     marginVertical: 20,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginBottom: 20,
   },
-    engagement: {
+  engagement: {
     fontSize: 18,
   },
 });
