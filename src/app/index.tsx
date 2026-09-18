52dc5a56ad0e40d3200cd510e45dd9c20819d172
mscoloma <MaryAnn.Coloma@edu.sait.ca>
2026-09-17 20:37:20 -0600

    changed final

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function PhotoViewer() {
   return (
